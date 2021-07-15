f50a70270bff0113fb2770fc2108f48817915d41
嶋津恒彦 <simanezumi1989@gmail.com>
2021-07-15 16:58:45 +0900

    タイムラインによるログの生成

---
+++ Assets/Scripts/NoteInstance.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteInstance : MonoBehaviour
+{
+    public void NoteEvent()
+    {
+        Debug.Log("音がなるよ");
+    }
+}
