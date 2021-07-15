7413a3941595d582a40a7cd03b1ebd0b0f475d46
嶋津恒彦 <simanezumi1989@gmail.com>
2021-07-15 16:34:31 +0900

    素材のインポートとタイムラインによる音の再生

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // 音ゲー講座
+    // 対象：これからUnity始める方でもOK（理想：他の講座をやったことがある）
+
+    // 身に付くこと
+    //　・音楽に合わせたノーツの生成
+    //　・当たり判定
+
+    // 環境
+    // ・Unity2020.3.6f1(2020あたりならOK)
+    // ・Visual Studio
+    // ・iMac（WinでもOK）
+
+}
